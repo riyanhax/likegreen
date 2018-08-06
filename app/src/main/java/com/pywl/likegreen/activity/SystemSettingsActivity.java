1058b86e2ce820cc77eb1ba4202eb1473ca6b87f
Gogs <gogs@fake.local>
2018-08-06 11:13:37 +0800

    创建dialog

--- app/src/main/java/com/pywl/likegreen/activity/SystemSettingsActivity.java
+++ app/src/main/java/com/pywl/likegreen/activity/SystemSettingsActivity.java
@@ -2,11 +2,13 @@ package com.pywl.likegreen.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.pywl.likegreen.R;
+import com.pywl.likegreen.view.ToPRomoteDialog;
 
-public class SystemSettingsActivity extends AppCompatActivity {
-
+public class SystemSettingsActivity extends AppCompatActivity implements View.OnClickListener {
+   private  View mMyToPromote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +20,24 @@ public class SystemSettingsActivity extends AppCompatActivity {
 
 
     private void initView() {
-
+        //我要推广
+        mMyToPromote = findViewById(R.id.my_to_promote);
+        mMyToPromote.setOnClickListener(this);
     }
     private void initData() {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.my_to_promote:
+                showDialog();
+                break;
+        }
+    }
+    private void showDialog(){
+        ToPRomoteDialog toPRomoteDialog = new ToPRomoteDialog(this);
+        toPRomoteDialog.show();
     }
 }
