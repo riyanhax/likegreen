7a8867863190f922c5359c8ac0f8b880b112cad2
fjjpydc <1161600367@qq.com>
2018-08-16 22:46:22 +0800

    8.16添加poupwindow

--- app/src/main/java/com/pywl/likegreen/fragment/RecommendedFragment.java
+++ app/src/main/java/com/pywl/likegreen/fragment/RecommendedFragment.java
@@ -1,11 +1,15 @@
 package com.pywl.likegreen.fragment;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupWindow;
 
 import com.pywl.likegreen.R;
 
@@ -15,6 +19,7 @@ import com.pywl.likegreen.R;
 //推荐
 public class RecommendedFragment extends Fragment implements View.OnClickListener {
     private View mIvRecommendShare;
+    private PopupWindow popupWindow;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -38,12 +43,17 @@ public class RecommendedFragment extends Fragment implements View.OnClickListene
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.iv_recommend_share:
-                showPopuWindow();
+                showPopuWindow(view);
                 break;
         }
     }
 
-    private void showPopuWindow() {
-
+    private void showPopuWindow(View view) {
+        View contentView = LayoutInflater.from(getActivity()).inflate(R.layout.pop_recommend_share, null);
+        popupWindow = new PopupWindow(contentView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setTouchable(true);
+        popupWindow.showAtLocation(view, Gravity.BOTTOM,0,0);
     }
 }
