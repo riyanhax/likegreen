a56c07e303a068069870e7ccb2eadb257479d781
Gogs <gogs@fake.local>
2018-09-05 18:15:43 +0800

    9.5测试版3

--- app/src/main/java/com/pywl/likegreen/fragment/find/ForumFragment.java
+++ app/src/main/java/com/pywl/likegreen/fragment/find/ForumFragment.java
@@ -3,11 +3,18 @@ package com.pywl.likegreen.fragment.find;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.flyco.tablayout.SlidingTabLayout;
 import com.pywl.likegreen.R;
+import com.pywl.likegreen.fragment.find.forum.ForumHotFragment;
+import com.pywl.likegreen.fragment.find.forum.ForumTheNewsFragment;
+import com.pywl.likegreen.fragment.find.forum.ForumTopicFragment;
+
+import java.util.ArrayList;
 
 /**
  * Created by theWind on 2018/8/1.
@@ -16,10 +23,23 @@ import com.pywl.likegreen.R;
 
 
 public class ForumFragment extends Fragment {
+    String[] items=new String[]{"话题","最新","热门"};
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_find_forum, container, false);
+        initView(view);
         return view;
+    }
+
+    private void initView(View v) {
+        SlidingTabLayout tabLayout =(SlidingTabLayout) v.findViewById(R.id.tab_forum);
+        ViewPager viewPager =(ViewPager)v.findViewById(R.id.viewpager_forum);
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new ForumTopicFragment());
+        fragments.add(new ForumTheNewsFragment());
+        fragments.add(new ForumHotFragment());
+        tabLayout.setViewPager(viewPager,items,getActivity(),fragments);
     }
 }
---
+++ app/src/main/java/com/pywl/likegreen/fragment/find/forum/ForumTheNewsFragment.java
@@ -0,0 +1,22 @@
+package com.pywl.likegreen.fragment.find.forum;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pywl.likegreen.R;
+
+/*
+* 话题
+* */
+public class ForumTheNewsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_find_forum, container, false);
+        return view;
+    }
+}
