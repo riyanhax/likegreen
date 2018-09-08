a2d6d52c33d31adc5c133e55f2927636caaefe63
Gogs <gogs@fake.local>
2018-09-08 12:01:56 +0800

    9.8发现弹框

--- app/src/main/java/com/pywl/likegreen/fragment/home/HomeFindFragment.java
+++ app/src/main/java/com/pywl/likegreen/fragment/home/HomeFindFragment.java
@@ -1,17 +1,25 @@
 package com.pywl.likegreen.fragment.home;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.flyco.tablayout.SlidingTabLayout;
+import com.flyco.tablayout.listener.OnTabSelectListener;
 import com.pywl.likegreen.R;
 import com.pywl.likegreen.base.HomeBottomBarFragment;
 import com.pywl.likegreen.fragment.find.ForumFragment;
@@ -26,13 +34,16 @@ import java.util.ArrayList;
  * Created by theWind on 2018/8/1.
  */
 
-public class HomeFindFragment extends HomeBottomBarFragment {
+public class HomeFindFragment extends HomeBottomBarFragment implements View.OnClickListener {
     private String[] items=new String[]{"精选","论坛","商城"};
     private SlidingTabLayout tabLayout;
     private ViewPager viewPager;
     private int tab;
     private Fragment selectedFragment,forumFragment,shoppingMallFragment;
     private RadioButton selected,forum,shoppingmall;
+    private ImageView equipment,shoppingCar;//应用设备  购物车
+    private View downView;
+    private boolean isShow=true;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -41,12 +52,99 @@ public class HomeFindFragment extends HomeBottomBarFragment {
         //initFragment();
         initData();
         return view;
-
     }
-    private void initView(View v) {
+
+
+        private void initView(View v) {
         tabLayout = (SlidingTabLayout)v.findViewById(R.id.st_home_find);
         viewPager = (ViewPager)v.findViewById(R.id.viewpager_home_find);
-/*        RadioGroup radioGroup = (RadioGroup)v.findViewById(R.id.rg_find);
+        equipment = (ImageView)v.findViewById(R.id.iv_find_equipment);//应用设备
+        equipment.setOnClickListener(this);
+        shoppingCar = (ImageView)v.findViewById(R.id.iv_find_shopping);//购物
+        downView = v.findViewById(R.id.find_downview);
+        v.findViewById(R.id.view_transparent).setOnClickListener(this);
+        }
+
+
+    private void initData() {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new SelectedFragment());
+        fragments.add(new ForumFragment());
+        fragments.add(new ShoppingMallFragment());
+        tabLayout.setViewPager(viewPager,items,getActivity(),fragments);
+        tabLayout.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+                switch (position){
+                    case 0:
+                    case 1:
+                        equipment.setVisibility(View.VISIBLE);
+                        shoppingCar.setVisibility(View.GONE);
+                        break;
+                    case 2:
+                        equipment.setVisibility(View.GONE);
+                        shoppingCar.setVisibility(View.VISIBLE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_find_equipment:
+                showdownView(isShow);
+                break;
+            case R.id.view_transparent:
+                showdownView(isShow);
+                break;
+        }
+    }
+
+    private void showdownView(boolean b) {
+        if (b){
+            downView.setVisibility(View.VISIBLE);
+            isShow=false;
+            Log.v("nihaoma",isShow+"1111111");
+        }else {
+            downView.setVisibility(View.GONE);
+            isShow=true;
+            Log.v("nihaoma",isShow+"2222222222222");
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*    private void initView(View v) {
+        tabLayout = (SlidingTabLayout)v.findViewById(R.id.st_home_find);
+        viewPager = (ViewPager)v.findViewById(R.id.viewpager_home_find);
+        equipment = (ImageView)v.findViewById(R.id.iv_find_equipment);
+        RadioGroup radioGroup = (RadioGroup)v.findViewById(R.id.rg_find);
          selected =(RadioButton) v.findViewById(R.id.selected);
          forum =(RadioButton) v.findViewById(R.id.forum);
          shoppingmall =(RadioButton) v.findViewById(R.id.shoppingmall);
@@ -55,8 +153,11 @@ public class HomeFindFragment extends HomeBottomBarFragment {
             public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
                 setTab(checkedId);
             }
-        });*/
-    }
+        });
+    }*/
+
+
+
 
 
 /*    private void initFragment() {
@@ -68,13 +169,7 @@ public class HomeFindFragment extends HomeBottomBarFragment {
         transaction.commitAllowingStateLoss();
     }*/
 
-    private void initData() {
-        ArrayList<Fragment> fragments = new ArrayList<>();
-        fragments.add(new SelectedFragment());
-        fragments.add(new ForumFragment());
-        fragments.add(new ShoppingMallFragment());
-        tabLayout.setViewPager(viewPager,items,getActivity(),fragments);
-    }
+
 
 /*    public void setTab(int tab) {
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
@@ -123,4 +218,4 @@ public class HomeFindFragment extends HomeBottomBarFragment {
         forum.setChecked(false);
         shoppingmall.setChecked(false);
     }*/
-}
+
