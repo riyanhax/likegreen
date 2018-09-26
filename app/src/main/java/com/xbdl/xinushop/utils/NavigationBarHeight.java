59854ea024c98396c16cd3a8fac8b275ebbd8b22
Gogs <gogs@fake.local>
2018-09-26 09:27:06 +0800

    9.26判断导航栏

--- app/src/main/java/com/xbdl/xinushop/utils/NavigationBarHeight.java
+++ app/src/main/java/com/xbdl/xinushop/utils/NavigationBarHeight.java
@@ -1,18 +1,23 @@
 package com.xbdl.xinushop.utils;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Point;
 import android.os.Build;
+import android.view.Display;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 
 import java.lang.reflect.Method;
 
 public class NavigationBarHeight {
     //获取虚拟按键的高度
-    public static int getNavigationBarHeight(Context context) {
+    public static int getNavigationBarHeight(Activity context) {
         int result = 0;
-        if (hasNavBar(context)) {
+        if (isNavigationBarShow(context)) {
             Resources res = context.getResources();
             int resourceId = res.getIdentifier("navigation_bar_height", "dimen", "android");
             if (resourceId > 0) {
@@ -46,6 +51,26 @@ public class NavigationBarHeight {
             return !ViewConfiguration.get(context).hasPermanentMenuKey();
         }
     }
+    public static boolean isNavigationBarShow(Activity activity){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Display display = activity.getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            Point realSize = new Point();
+            display.getSize(size);
+            display.getRealSize(realSize);
+            boolean  result  = realSize.y!=size.y;
+            return realSize.y!=size.y;
+        }else {
+            boolean menu = ViewConfiguration.get(activity).hasPermanentMenuKey();
+            boolean back = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK);
+            if(menu || back) {
+                return false;
+            }else {
+                return true;
+            }
+        }
+    }
+
 
     /**
      * 判断虚拟按键栏是否重写
