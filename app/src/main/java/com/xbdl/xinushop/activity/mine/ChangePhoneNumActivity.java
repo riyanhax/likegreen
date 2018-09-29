f58c9972dac2de54d78675752126a99609db670b
Gogs <gogs@fake.local>
2018-09-29 18:33:04 +0800

    9.29fjj修改找回个人信息，完成地址

--- app/src/main/java/com/xbdl/xinushop/activity/mine/ChangePhoneNumActivity.java
+++ app/src/main/java/com/xbdl/xinushop/activity/mine/ChangePhoneNumActivity.java
@@ -1,15 +1,199 @@
 package com.xbdl.xinushop.activity.mine;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
+import com.lzy.okgo.request.base.Request;
 import com.xbdl.xinushop.R;
+import com.xbdl.xinushop.activity.LoginActivity;
+import com.xbdl.xinushop.activity.RegisterActivity;
+import com.xbdl.xinushop.base.BaseActivity;
+import com.xbdl.xinushop.utils.HttpUtils;
 
-public class ChangePhoneNumActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ChangePhoneNumActivity extends BaseActivity implements View.OnClickListener {
+    private EditText mPhoneNumber, et_code, etPwd;
+    private TextView sendCode, tv_register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_phone_num);
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.iv_return).setOnClickListener(this);
+        mPhoneNumber = (EditText) findViewById(R.id.et_register_phone);
+        et_code = (EditText) findViewById(R.id.et_register_code);
+        etPwd = (EditText) findViewById(R.id.et_register_pwd);
+        sendCode = (TextView) findViewById(R.id.tv_send_code);
+        sendCode.setOnClickListener(this);
+        tv_register = (TextView) findViewById(R.id.tv_register);//注册
+        tv_register.setOnClickListener(this);
+    }
+
+    //忘记密码
+    private void forgetpwd(String phonenum, String pwd, String code) {
+        HttpUtils.forget(phonenum, pwd, code, new StringCallback() {
+            @Override
+            public void onSuccess(Response<String> response) {
+                String body = response.body();
+                try {
+                    JSONObject jsonObject = new JSONObject(body);
+                    int code = jsonObject.getInt("code");
+                    String object = jsonObject.getString("object");
+                    if (code == 100) {
+                        Toast.makeText(ChangePhoneNumActivity.this, object, Toast.LENGTH_SHORT).show();
+                        //加密
+                        //String phoneEncode = AESUtils.encryptString(phonenum, MyConstants.Key);
+                        //String pwdEncode = AESUtils.encryptString(pwd, MyConstants.Key);
+                        // SharedPreferencesUtil.putString(RegisterActivity.this,MyConstants.PHONE,phoneEncode);
+                        // SharedPreferencesUtil.putString(RegisterActivity.this,MyConstants.PASSWORD,pwdEncode);
+                        dismissLoading();
+                        Intent intentMainActivity = new Intent(ChangePhoneNumActivity.this, LoginActivity.class);
+                        startActivity(intentMainActivity);
+                        // time.reset();
+                        finish();
+                    } else {
+
+                        Toast.makeText(ChangePhoneNumActivity.this, object, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onStart(Request<String, ? extends Request> request) {
+                super.onStart(request);
+                showLoading();
+                Log.v("nihaoma", "4444444444");
+            }
+
+            @Override
+            public void onError(Response<String> response) {
+                super.onError(response);
+                dismissLoading();
+                Log.v("nihaoma", response + "55555555555");
+            }
+
+            @Override
+            public void onFinish() {
+                super.onFinish();
+                dismissLoading();
+            }
+        });
+    }
+
+    @Override
+    protected Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_return:
+                finish();
+                break;
+            case R.id.tv_send_code:
+                if (mPhoneNumber.length() != 11) {
+                    Toast.makeText(this, "请输入正确手机号", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                sendCode();
+
+                break;
+            case R.id.tv_register:
+                register();
+                break;
+        }
+
+    }
+
+    private void register() {
+        if (mPhoneNumber.length() != 11) {
+            Toast.makeText(this, "请输入正确手机号", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(etPwd.getText().toString())) {
+            Toast.makeText(this, "请输入密码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(et_code.getText().toString())) {
+            Toast.makeText(this, "请输入验证码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String phonenum = mPhoneNumber.getText().toString();
+        String pwd = etPwd.getText().toString();
+        String code = et_code.getText().toString();
+        if (mPhoneNumber.length() == 11 && !TextUtils.isEmpty(etPwd.toString()) && !TextUtils.isEmpty(et_code.toString())) {
+            forgetpwd(phonenum, pwd, code);
+        }
+    }
+
+    private void sendCode() {
+        if (mPhoneNumber.length() != 11) {
+            Toast.makeText(ChangePhoneNumActivity.this, "请输入正确手机号", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        HttpUtils.sendCode(mPhoneNumber.getText().toString(), new StringCallback() {
+            @Override
+            public void onSuccess(Response<String> response) {
+                String body = response.body();
+                try {
+                    JSONObject jsonObject = new JSONObject(body);
+                    int code = jsonObject.getInt("code");
+                    if (code == 100) {
+                        Toast.makeText(ChangePhoneNumActivity.this, "短信已发送", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(ChangePhoneNumActivity.this, "发送不成功", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStart(Request<String, ? extends Request> request) {
+                super.onStart(request);
+                showLoading();
+                Log.v("nihaoma", "4444444444");
+            }
+
+            @Override
+            public void onError(Response<String> response) {
+                super.onError(response);
+                dismissLoading();
+                Log.v("nihaoma", response + "55555555555");
+            }
+
+            @Override
+            public void onFinish() {
+                super.onFinish();
+                dismissLoading();
+            }
+        });
     }
 }
--- app/src/main/java/com/xbdl/xinushop/activity/mine/PersonalDataActivity.java
+++ app/src/main/java/com/xbdl/xinushop/activity/mine/PersonalDataActivity.java
@@ -1,34 +1,49 @@
 package com.xbdl.xinushop.activity.mine;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.lzy.imagepicker.ImagePicker;
+import com.lzy.imagepicker.bean.ImageItem;
 import com.lzy.imagepicker.ui.ImageGridActivity;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
+import com.lzy.okgo.request.base.Request;
+import com.xbdl.xinushop.MyApplication;
 import com.xbdl.xinushop.R;
 import com.xbdl.xinushop.base.BaseActivity;
 import com.xbdl.xinushop.base.BasePresenter;
+import com.xbdl.xinushop.bean.JsonRootBean;
+import com.xbdl.xinushop.constant.UrlConstant2;
+import com.xbdl.xinushop.utils.HttpUtils2;
+import com.xbdl.xinushop.utils.ImageUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 //个人资料页
 public class PersonalDataActivity extends BaseActivity implements View.OnClickListener {
+    private ArrayList<ImageItem> headicon;
     private View mSystemPersonalHead,mSystemPersonalName,mSystemPersonPhone,mRlSagnatrue,mRlShoppingAddress;
     private CircleImageView mIvPersonalHead;
     private TextView mTvPersonalName,mTvPersonalPhone;
 
     @Override
-    protected Activity getActivity() {
-        return PersonalDataActivity.this;
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_data);
+        ImagePicker.getInstance().setMultiMode(false);
         initView();
         initData();
     }
@@ -41,13 +56,13 @@ public class PersonalDataActivity extends BaseActivity implements View.OnClickLi
         mIvPersonalHead = (CircleImageView)findViewById(R.id.iv_personal_head);
         //昵称栏
         mSystemPersonalName = findViewById(R.id.system_personal_data_nechen);
-        mSystemPersonalHead.setOnClickListener(this);
+        mSystemPersonalName.setOnClickListener(this);
         //昵称
         mTvPersonalName=(TextView)findViewById(R.id.tv_personal_name);
         //手机号栏
-         mSystemPersonPhone = findViewById(R.id.system_personal_phone);
-         mSystemPersonPhone.setOnClickListener(this);
-         //手机号
+        mSystemPersonPhone = findViewById(R.id.system_personal_phone);
+        mSystemPersonPhone.setOnClickListener(this);
+        //手机号
         mTvPersonalPhone=(TextView)findViewById(R.id.tv_personal_phone);
         //个性化签名
         findViewById(R.id.rl_sagnatrue).setOnClickListener(this);
@@ -55,39 +70,129 @@ public class PersonalDataActivity extends BaseActivity implements View.OnClickLi
         mRlShoppingAddress.setOnClickListener(this);
         findViewById(R.id.rl_idcard_check).setOnClickListener(this);//身份证验证
         findViewById(R.id.rl_business_check).setOnClickListener(this);//营业执照验证
+        findViewById(R.id.iv_return).setOnClickListener(this);//返回
+
     }
 
     private void initData() {
+        //设置手机号
+        mTvPersonalPhone.setText(MyApplication.user.getUserPhone());
 
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.iv_personal_head:
             case R.id.system_personal_head:
+                Intent intentPerview = new Intent(this, ImageGridActivity.class);
+                intentPerview.putExtra(ImageGridActivity.EXTRAS_IMAGES,headicon);
+                startActivityForResult(intentPerview, 100);
                 break;
             case R.id.system_personal_phone://修改手机号码
-                Intent intentChangePhoneNumActivity = new Intent(this, com.xbdl.xinushop.activity.mine.ChangePhoneNumActivity.class);
+                Intent intentChangePhoneNumActivity = new Intent(this, ChangePhoneNumActivity.class);
                 startActivity(intentChangePhoneNumActivity);
                 break;
             case R.id.rl_sagnatrue://个性签名
-                Intent intentChangeUserMsgActivity = new Intent(this, com.xbdl.xinushop.activity.mine.AutographActivity.class);
-                startActivity(intentChangeUserMsgActivity);
-            case R.id.rl_shopping_address://个性签名
-                Intent intentShoppingAddressActivity = new Intent(this, com.xbdl.xinushop.activity.mine.ShoppingAddressActivity.class);
+                Intent intentChangeUserMsgActivity = new Intent(this, AutographActivity.class);
+                intentChangeUserMsgActivity.putExtra("PersonalDataActivity",0);
+                startActivityForResult(intentChangeUserMsgActivity,200);
+                break;
+            case R.id.rl_shopping_address://地址
+                Intent intentShoppingAddressActivity = new Intent(this, ShoppingAddressActivity.class);
                 startActivity(intentShoppingAddressActivity);
                 break;
             case R.id.rl_idcard_check://身份证验证
-                Intent intentIDCardCheckActivity = new Intent(this, com.xbdl.xinushop.activity.mine.IDCardCheckActivity.class);
+                Intent intentIDCardCheckActivity = new Intent(this,IDCardCheckActivity.class);
                 startActivity(intentIDCardCheckActivity);
                 break;
             case R.id.rl_business_check://营业执照验证
-                Intent intentBusinessLicenseActivity = new Intent(this, com.xbdl.xinushop.activity.mine.BusinessLicenseActivity.class);
+                Intent intentBusinessLicenseActivity = new Intent(this, BusinessLicenseActivity.class);
                 startActivity(intentBusinessLicenseActivity);
                 break;
-
+            case R.id.system_personal_data_nechen://设置名字
+                Intent intentChangeUserMsgActivity2 = new Intent(this, AutographActivity.class);
+                intentChangeUserMsgActivity2.putExtra("PersonalDataActivity",1);
+                startActivityForResult(intentChangeUserMsgActivity2,200);
+                break;
+            case R.id.iv_return://返回
+                finish();
+                break;
         }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == ImagePicker.RESULT_CODE_ITEMS) {
+            if (data != null && requestCode == 100) {
+                headicon = (ArrayList<ImageItem>) data.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
+                String bitmapToString = ImageUtils.bitmapToString(headicon.get(0).path);
+                sendMsg("headPortrait",bitmapToString);
+                Glide.with(this).load(headicon.get(0).path).into(mIvPersonalHead);
+            } else {
+                Toast.makeText(this, "没有数据", Toast.LENGTH_SHORT).show();
+            }
+        }else if (resultCode == RESULT_OK){
+            if (data!=null&&requestCode==200){
+                String sagnetrue = data.getStringExtra("sagnetrue");
+                if (sagnetrue!=null){
+                    Log.v("nihaoma",sagnetrue);
+
+                    sendMsg("signature",sagnetrue);
+                }
+                String nichen = data.getStringExtra("nichen");
+                if (nichen!=null){
+                    Log.v("nihaoma",nichen);
+                    mTvPersonalName.setText(nichen);
+                    sendMsg("userName",nichen);
+                }
+            }
+        }
+
     }
 
 
+    private void sendMsg(String key,String val){
+        String loginToken = MyApplication.user.getLoginToken();
+        Log.v("nihaoma",loginToken+key+val+ UrlConstant2.updataUser);
+        HttpUtils2.updataUser(loginToken,key,val, new StringCallback() {
+            @Override
+            public void onSuccess(Response<String> response) {
+                Log.v("nihaoma","4444444");
+                Gson gson = new Gson();
+                JsonRootBean jsonRootBean = gson.fromJson(response.body(), JsonRootBean.class);
+                Log.v("nihaoma",jsonRootBean.toString());
+                dismissLoading();
+            }
+
+            @Override
+            public void onStart(Request<String, ? extends Request> request) {
+                super.onStart(request);
+                Log.v("nihaoma","1111111");
+                showLoading();
+            }
+
+            @Override
+            public void onError(Response<String> response) {
+                super.onError(response);
+                Log.v("nihaoma","22222222"+response.body());
+
+                dismissLoading();
+            }
+
+            @Override
+            public void onFinish() {
+                super.onFinish();
+                Log.v("nihaoma","3333333");
+                dismissLoading();
+            }
+        });
+
+    }
+
+    @Override
+    protected Activity getActivity() {
+        return this;
+    }
 }
